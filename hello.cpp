7fd8ec9a3de6a9fa91267be59d0ec971905e5b11
hridyansh5492 <hridyanshsati4321@gmail.com>
2025-03-31 17:23:16 +0530

    first program of cpp hello

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout<<"hello"<<std::endl;
+    std::cout<<"world"<<std::endl;
+
+    return 0;
+}
+
+//endl flushes output buffer but \n doesn't 
+//comment are same as c
